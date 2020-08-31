01894808b536929001d76d2e8a75a0cb983410c8
Amin Limbada <amin.limbada@consensys.net>
2020-08-31 21:01:56 +0400

    Add 'react-loading-overlay' to show loader and 'react-infinte-scroll-component' to load set of 30 stories everytime the user hits the bottom of the window, till no more stories are left to load.

--- src/components/Story/Story.jsx
+++ src/components/Story/Story.jsx
@@ -7,32 +7,27 @@ import Typography from "@material-ui/core/Typography";
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
 import { getStory } from "../../requests/hackerNewsAPI";
+import LoadingOverlay from "react-loading-overlay";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    minWidth: 275,
+    background: "#fafafa",
+    boxShadow:
+      "0px 2px 4px rgba(0, 0, 0, 0.14), 0px 3px 4px rgba(0, 0, 0, 0.12), 0px 1px 5px rgba(0, 0, 0, 0.2)",
+    borderRadius: "10px",
+    minHeight: "300px",
+    marginTop: "25px",
+    padding: "0 5%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
   },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
-  },
+
   title: {
     fontSize: 14,
+    fontWeight: "bold",
   },
-  pos: {
-    marginBottom: 12,
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest,
-    }),
-  },
-  expandOpen: {
-    transform: "rotate(180deg)",
-  },
+
   avatar: {
     backgroundColor: "red",
   },
@@ -40,45 +35,52 @@ const useStyles = makeStyles((theme) => ({
 
 const Story = ({ id }) => {
   useEffect(() => {
+    setLoading(true);
     getStory(id).then((story) => setStory(story));
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const classes = useStyles();
   const [story, setStory] = useState({});
+  const [loading, setLoading] = useState(false);
 
   return (
-    <Card>
-      <CardContent>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              {story && story.title && story.title[0]}
-            </Avatar>
-          }
-          title={story.title}
-          subheader={new Date(story.time * 1000).toDateString()}
-        />
-        <Typography className={classes.title} gutterBottom>
-          {story.title}
-        </Typography>
+    <LoadingOverlay active={loading} spinner text="Loading story">
+      <Card className={classes.root}>
+        <CardContent>
+          <CardHeader
+            avatar={
+              <Avatar aria-label="stories" className={classes.avatar}>
+                {story && story.title && story.title[0]}
+              </Avatar>
+            }
+            title={story.title}
+            subheader={new Date(story.time * 1000).toDateString()}
+          />
+          <Typography className={classes.title} gutterBottom>
+            {story.title}
+          </Typography>
 
-        <Typography variant="body2" component="p" color="textSecondary">
-          by - {story.by}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Typography>
-          <a
-            style={{ display: "table-cell" }}
-            href={story.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Go To Story
-          </a>
-        </Typography>
-      </CardActions>
-    </Card>
+          <Typography variant="body2" component="p" color="textSecondary">
+            by - {story.by}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Typography>
+            <a
+              style={{ display: "table-cell" }}
+              href={story.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Check it out!
+            </a>
+          </Typography>
+        </CardActions>
+      </Card>
+    </LoadingOverlay>
   );
 };
 
--- src/scenes/Stories/Stories.jsx
+++ src/scenes/Stories/Stories.jsx
@@ -1,10 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import { getStoryIDs } from "../../requests/hackerNewsAPI";
 import Story from "../../components/Story/Story";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { useLocation } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 const StoryContainer = () => {
   const [storyIDs, setStoryIDs] = useState([]);
+  const [noOfStories, setnoOfStories] = useState(0);
+  const [count, setCount] = useState(30);
   const location = useLocation();
 
   useEffect(() => {
@@ -15,16 +20,47 @@ const StoryContainer = () => {
     if (location.pathname === "/new") {
       urlSuffix = "/newstories";
     }
-    getStoryIDs(urlSuffix).then((data) => setStoryIDs(data.slice(0, 6)));
-  }, [location]);
+    getStoryIDs(urlSuffix).then((data) => {
+      setStoryIDs(data.slice(0, count));
+      setnoOfStories(data.length);
+    });
+  }, [location, count]);
   return (
-    <Grid container spacing={4}>
-      {storyIDs.map((id) => (
-        <Grid item xs={12} md={4} key={id}>
-          <Story id={id} />
-        </Grid>
-      ))}
-    </Grid>
+    <InfiniteScroll
+      dataLength={storyIDs.length}
+      next={() => setCount(count + 30)}
+      hasMore={count < noOfStories}
+      loader={
+        <Typography
+          style={{
+            textAlign: "center",
+            margin: "10px 0",
+          }}
+        >
+          Loading more stories
+        </Typography>
+      }
+      endMessage={
+        <Typography
+          style={{
+            textAlign: "center",
+            margin: "10px 0",
+            textDecoration: "underline",
+          }}
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          <b>Yay! You have seen it all. Click here to scroll back to the top</b>
+        </Typography>
+      }
+    >
+      <Grid container spacing={4}>
+        {storyIDs.map((id) => (
+          <Grid item xs={12} md={4} key={id}>
+            <Story id={id} />
+          </Grid>
+        ))}
+      </Grid>
+    </InfiniteScroll>
   );
 };
 
