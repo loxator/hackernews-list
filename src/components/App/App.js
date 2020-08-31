168f4b604e31367feff69bcd95656a3d087e1993
Amin Limbada <amin.limbada@consensys.net>
2020-08-31 19:05:42 +0400

    Add react-router-dom and implement navigation

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,19 +2,31 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import TopNav from "../TopNav/TopNav";
 import Stories from "../../scenes/Stories/Stories";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 const App = () => {
   return (
-    <Grid container direction="column">
-      <TopNav />
-      <Grid item container>
-        <Grid item xs={0} sm={2} />
-        <Grid item xs={12} sm={8}>
-          <Stories />
+    <Router>
+      <Grid container direction="column">
+        <TopNav />
+        <Grid item container>
+          <Grid item xs={false} sm={2} />
+          <Grid item xs={12} sm={8}>
+            <Switch>
+              <Redirect exact from="/" to="/top" />
+              <Route exact path={["/top", "/best", "/new"]}>
+                <Stories />
+              </Route>
+            </Switch>
+          </Grid>
+          <Grid item xs={false} sm={2} />
         </Grid>
-        <Grid item xs={0} sm={2} />
       </Grid>
-    </Grid>
+    </Router>
   );
 };
 
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -4,6 +4,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import { NavLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {
@@ -11,22 +12,61 @@ const useStyles = makeStyles((theme) => ({
   },
   linkContainer: { flexGrow: 1, margin: "auto" },
   logoContainer: { flexGrow: 1, flexBasis: "75%" },
-  listStyle: {
-    display: "flex",
+  title: {
+    flexGrow: 1,
+  },
+  nav: {
+    flexGrow: 1,
+  },
+  links: {
     listStyle: "none",
+    display: "flex",
     justifyContent: "space-around",
-    color: "#fff",
+    "& > li a": {
+      color: "white",
+      textDecoration: "none",
+      "&.active": {
+        textDecoration: "underline",
+      },
+    },
   },
 }));
 
 const TopNav = () => {
   const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}>
           Hacker News!
         </Typography>
+
+        <nav className={classes.nav}>
+          <ul className={classes.links}>
+            <li>
+              <NavLink to="/top">
+                <Typography variant="h6" className={classes.title}>
+                  Top
+                </Typography>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/best">
+                <Typography variant="h6" className={classes.title}>
+                  Best
+                </Typography>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/new">
+                <Typography variant="h6" className={classes.title}>
+                  New
+                </Typography>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </Toolbar>
     </AppBar>
   );
